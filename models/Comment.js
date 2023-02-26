2debfcda77ee47a81a49fb3c146e60b7f4efe13e
sanchezju <sjuan634@yahoo.com>
2023-02-25 20:07:55 -0800

    setup schema for user post comment

---
+++ models/Comment.js
@@ -0,0 +1,18 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+class Comment extends Model {};
+
+Comment.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    comment: {
+      type: DataTypes.TEXT,
+    },
+  },
+);
