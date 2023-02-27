e64e951733e2e7702aae78f44bc6e69c23134578
sanchezju <sjuan634@yahoo.com>
2023-02-26 18:26:14 -0800

    adjust routes to render handlebars

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -1,5 +1,5 @@
 const router = require('express').Router();
-const { User, Post, Comment } = require('../models');
-const withAuth = require('../utils/auth');
+const { User, Post, Comment } = require('../../models');
+const withAuth = require('../../utils/auth');
 
 module.exports = router;
--- controllers/index.js
+++ controllers/index.js
@@ -1,8 +1,8 @@
 const router = require('express').Router();
 
-// const apiRoutes = require('./api');
+const apiRoutes = require('./api');
 const homeRoutes = require('./homeRoutes');
-// const dashboardRoutes = require('./dashboardRoutes.js');
+const dashboardRoutes = require('./dashboardRoutes.js');
 
 router.use('/', homeRoutes);
 router.use('/dashboard', dashboardRoutes);
