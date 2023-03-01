cec791dfd43de8f61323692f0cba410545a11297
sanchezju <sjuan634@yahoo.com>
2023-02-28 22:42:13 -0800

    add login, sign up, and single post routes

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -10,20 +10,11 @@ router.get('/', async (req, res) => {
           model: User,
           attributes: ['name'],
         },
-        {
-          model: Comment,
-          attributes: ['comment'],
-          include: [
-            {
-              model: User,
-              attributes: ['name']
-            }
-          ]
-        },
       ]
     });
     const posts = postData.map((post) => post.get({ plain: true }));
 
+    console.log(posts);
     res.render('homepage', {
       posts,
       logged_in: req.session.logged_in,
@@ -32,6 +23,62 @@ router.get('/', async (req, res) => {
     console.log(err);
     res.status(500).json(err);
   }
+});
+
+router.get('/post/:id', async (req, res) => {
+  try {
+    const postData = await Post.findByPk(req.params.id, {
+      include: [
+        {
+          model: User,
+          attributes: ['name'],
+        },
+        {
+          model: Comment,
+          attributes: ['comment', 'createdAt'],
+          include: [
+            {
+              model: User,
+              attributes: ['name'],
+            }
+          ]
+        },
+      ]
+    });
+
+    if (postData) {
+      const post = postData.get({plain: true});
+      
+      console.log(post);
+      res.render('post', {
+        post,
+        logged_in: req.session.logged_in,
+      });
+    } else {
+      res.status(404).end();
+    }
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
+  }
+});
+
+router.get('/login', (req, res) => {
+  if (req.session.loggedIn) {
+    res.redirect('/');
+    return;
+  }
+
+  res.render('login');
+});
+
+router.get('/signup', (req, res) => {
+  if (req.session.loggedIn) {
+    res.redirect('/');
+    return;
+  }
+
+  res.render('signup');
 });
 
 module.exports = router;
