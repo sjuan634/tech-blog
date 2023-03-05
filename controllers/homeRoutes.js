b20de5f334c4caeac258a9bfb92ac5c917ca7ade
sanchezju <sjuan634@yahoo.com>
2023-03-04 18:28:23 -0800

    add dashboard route

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -25,6 +25,25 @@ router.get('/', async (req, res) => {
   }
 });
 
+router.get('/dashboard', withAuth, async (req, res) => {
+  try {
+    const userData = await User.findByPk(req.session.user_id, {
+      attributes: { exclude: ['password'] },
+      include: [{ model: Post }],
+    });
+
+    const user = userData.get({ plain: true });
+    console.log(user);
+    res.render('dashboard', {
+      user,
+      logged_in: req.session.logged_in,
+    })
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
+  }
+});
+
 router.get('/post/:id', async (req, res) => {
   try {
     const postData = await Post.findByPk(req.params.id, {
