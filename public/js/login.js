cb9d11ecfb8a1732f2cb6b794f8830af28036342
sanchezju <sjuan634@yahoo.com>
2023-03-04 19:51:52 -0800

    remove signup logic to separate file

--- public/js/login.js
+++ public/js/login.js
@@ -1,13 +1,13 @@
 const loginFormHandler = async (event) => {
     event.preventDefault();
   
-    const username = document.querySelector('#username-input').value.trim();
+    const name = document.querySelector('#username-input').value.trim();
     const password = document.querySelector('#password-input').value.trim();
   
-    if (username && password) {
+    if (name && password) {
       const response = await fetch('/api/users/login', {
         method: 'POST',
-        body: JSON.stringify({ username, password }),
+        body: JSON.stringify({ name, password }),
         headers: { 'Content-Type': 'application/json' },
       });
   
@@ -21,34 +21,7 @@ const loginFormHandler = async (event) => {
     }
   };
   
-  const signupFormHandler = async (event) => {
-    event.preventDefault();
-  
-    const username = document.querySelector('#new-username-input').value.trim();
-    const password = document.querySelector('#new-password-input').value.trim();
-  
-    if (username && password) {
-      const response = await fetch('/api/users', {
-        method: 'POST',
-        body: JSON.stringify({ username, password }),
-        headers: { 'Content-Type': 'application/json' },
-      });
-  
-      if (response.ok) {
-        localStorage.setItem('back', document.referrer);
-        const back = localStorage.getItem('back');
-        location.href = back;  
-      } else {
-        alert('Failed to sign up.');
-      }
-    }
-  };
-  
   document
-    .querySelector('.login-form')
+    .querySelector('#login-form')
     .addEventListener('submit', loginFormHandler);
-  
-  document
-    .querySelector('.signup-form')
-    .addEventListener('submit', signupFormHandler);
   
