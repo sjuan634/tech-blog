e108801cf82ca89f5211970d72dfbe7dcb48a5b9
sanchezju <sjuan634@yahoo.com>
2023-03-05 22:14:06 -0800

    fix typo

--- controllers/api/postRoutes.js
+++ controllers/api/postRoutes.js
@@ -3,10 +3,10 @@ const { Post } = require('../../models');
 const withAuth = require('../../utils/auth');
 
 router.post('/', withAuth, async (req, res) => {
-  const body = req.body;
-
   try {
-    const newPost = await Post.create({ ...body, user_id: req.session.user_id });
+    const newPost = await Post.create({
+      ...req.body,
+      user_id: req.session.user_id });
     res.json(newPost);
   } catch (err) {
     res.status(500).json(err);
